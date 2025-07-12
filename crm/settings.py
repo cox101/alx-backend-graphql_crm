e2b072e90d412b10e94e306f4dee86800d518e28
cox101 <104098884+cox101@users.noreply.github.com>
2025-07-12 14:54:55 +0300

    commit

--- crm/settings.py
+++ crm/settings.py
@@ -18,3 +18,9 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(day_of_week='mon', hour=6, minute=0),
     },
 }
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
